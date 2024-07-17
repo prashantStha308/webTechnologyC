a770c5e73fb6ff6ce3a0cab8980b378e38ac2140
prashantStha308 <prashant.795424@cct.tu.edu.np>
2024-07-17 16:09:35 +0545

    All wrapped up

--- script.js
+++ script.js
@@ -45,46 +45,102 @@ const getReceiverName = ()=>{
     }
 }
 
-function displayImage(fileInput, imgContainer) {
-    const file = fileInput.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            imgContainer.style.backgroundImage = `url(${e.target.result})`;
-        };
-        reader.readAsDataURL(file);
+function displayImage( inImage , outImage , inputVal ){
+    const input = inImage.files[0];
+    outImage.textContent = "";
+    outImage.style.border = "none";
+
+    const image = document.createElement("img");
+    image.classList.add("iconImg");
+
+    const reader = new FileReader();
+    reader.onload = function(e){
+        image.setAttribute("src", e.target.result);
     }
+    reader.readAsDataURL(input);
+
+    outImage.appendChild(image);
+
+    if( inputVal === 1 ){
+        image.id = "iconImg1"
+    }else if( inputVal === 2 ){
+        image.id = "iconImg2";
+    }
+
 }
 
-function getIcon1() {
+function getIcon1(input){
     const icon1 = document.getElementById("icon1");
     const giveIcon1 = document.getElementById("giveIcon1");
-    displayImage(icon1, giveIcon1);
+
+    displayImage( icon1 , giveIcon1 , input );
 }
 
-function getIcon2() {
+function getIcon2(input){
     const icon2 = document.getElementById("icon2");
     const giveIcon2 = document.getElementById("giveIcon2");
-    displayImage(icon2, giveIcon2);
+
+    displayImage( icon2 , giveIcon2 , input );
+
 }
 
 const getIcon = ( input )=>{
-
     switch(input){
         case 1:
-            getIcon1();
+            getIcon1(input);
             break;
         
         case 2:
-            getIcon2();
+            getIcon2(input);
             break;
         default:
             console.log("Invalid");
     }
 }
 
-const printCertificate = (printable) =>{
+function reset1(){
 
+    const image = document.getElementById("iconImg1");
+    
+    if( !image ){
+        const containerDiv = document.getElementById("giveIcon1");
+        containerDiv.style.border = "none";
+        containerDiv.textContent = "";
+
+    }else{
+        image.remove();
+    }
+}
+
+function reset2(){
+
+    const image = document.getElementById("iconImg2");
+    
+    if( !image ){
+        const containerDiv = document.getElementById("giveIcon2");
+        containerDiv.style.border = "none";
+        containerDiv.textContent = "";
+
+    }else{
+        image.remove();
+    }
+}
+
+const reset = (input)=>{
+    switch(input){
+        case 1:
+            reset1();
+            break;
+        
+        case 2:
+            reset2();
+            break;
+        default:
+            console.log("Invalid");
+    }
+}
+
+
+const printCertificate = () =>{
     print();
-
 }
